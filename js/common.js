f8f51c5e97cd6e2bf038a729ec32d838b1533b26
alexei <soloveyalexey3@gmail.com>
2020-01-15 19:59:17 +0200

    fixed bug with github

--- js/common.js
+++ js/common.js
@@ -8,7 +8,12 @@ let temperature = document.getElementById('temp');
 let wind = document.getElementById('wind');
 let status = document.getElementById('status');
 
-fetch(url)
+fetch(url, {
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    })
   .then(response => response.json())
   .then(
     commits => {
